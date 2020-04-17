cdd08229ba574597e0189c09ca3b1fb601483d9b
Ayax <casarrubias2837@gmail.com>
2020-04-17 13:04:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ let session = require('express-session');
 let flash = require('express-flash');
 let passport = require('passport');
 
+const axios = require('axios');
+
 // Express app creation
 const app = express();
 
+//Socket.io
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 // Configurations
 const appConfig = require('./configs/app');
@@ -48,9 +53,37 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
+app.use('/', express.static(__dirname + '/public'))
 app.use('/', webRoutes);
 
+let player_names = []
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+io.on('connection', (socket) => {
+  axios.get("http://names.drycodes.com/1?nameOptions=starwarsFirstNames")
+  .then((response) => {
+    //console.log("axios response: ", response.data);
+    var player = response.data[0];
+      player_names.push(player);
+      socket.emit('init', {player : player, player_names : player_names});
+      socket.broadcast.emit('player added', {player_names : player_names});
+    });
+
+    socket.on('play', () => {
+      letter = letters[Math.floor((Math.random() * 26))];
+      socket.emit('play', {letter : letter});
+      socket.broadcast.emit('play', {letter : letter});
+    });
+
+    socket.on('finish', () => {
+      socket.emit('gradeAnswers');
+      socket.broadcast.emit('gradeAnswers');
+    })
+
+})
+
 // App init
-app.listen(appConfig.expressPort, () => {
+server.listen(appConfig.expressPort, () => {
   console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
 });
